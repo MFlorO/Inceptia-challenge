ac7230806542e27cbf72cc36eeee3e8d190b4ea0
Flori O <floroldani@hotmail.com>
2024-05-20 23:31:25 -0300

    agregar mensaje cuando no devuelve nada un rango de fechas

--- frontend/src/components/details/Details.tsx
+++ frontend/src/components/details/Details.tsx
@@ -10,25 +10,23 @@ const Details = () => {
     <Flex w='100%' minH='380px' h='max-content'>
       {
         details 
-        ? <Flex w='100%' direction='column' gap={4}>
-            <Flex color='white' alignItems='center' bgColor='#294855' gap={4} fontSize='12px'>
-              <Text>TODOS</Text>
-              <Text>TRANSFERIDOS</Text>
-              <Text>CLIENTE NO ENCONTRADO EN LA BD</Text>
-              <Text>LLAMANDO</Text>
-              <Text>CORTO CLIENTE</Text>
-              <Text>MAIL ENVIADO</Text>
-              <Text>INDEFINIDO</Text>
-              <Text>NO ENCONTRADO EN LA BD</Text>
-            </Flex>
-            { detailsLength && 
-              <Flex color='white' justifyContent='end' alignItems='center' fontSize='15px'>
-                {detailsLength} resultados
+        ? details?.length > 0 
+            ? <Flex w='100%' direction='column' gap={4}>
+                <Flex color='white' alignItems='center' justifyContent='space-between' bgColor='#294855' gap={4} fontSize='12px'>
+                  <Text>TODOS</Text>
+                  <Text>TRANSFERIDOS</Text>
+                  <Text>CLIENTE NO ENCONTRADO EN LA BD</Text>
+                  <Text>LLAMANDO</Text>
+                  <Text>CORTO CLIENTE</Text>
+                  <Text>MAIL ENVIADO</Text>
+                  <Text>INDEFINIDO</Text>
+                  <Text>NO ENCONTRADO EN LA BD</Text>
+                </Flex>
+                { detailsLength && <Flex color='white' justifyContent='end' alignItems='center' fontSize='15px'>{detailsLength} resultados</Flex> }
+                { details?.length > 0 && <TableDetails details={details} />}
               </Flex>
-            }
-            <TableDetails details={details} />
-          </Flex>
-        : <Text color='white' textAlign='center'>NO HAY RESULTADOS EN ESE RANGO DE FECHAS</Text>
+            : <Text w='100%' color='white' textAlign='center'>NO HAY RESULTADOS EN ESE RANGO DE FECHAS</Text>
+        : <Text w='100%' color='white' textAlign='center'>SELECCIONE UN RANGO DE FECHAS PARA VER MÁS INFORMACIÓN</Text>
       }
     </Flex>
   )
